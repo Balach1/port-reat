9150c82e96a35fa6f251fe6c7782668851952afb
Balach1 <balachsubra@gmail.com>
2022-09-21 23:00:34 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div>
       <h1 className="text-2xl text-rose-500">App</h1>
       <h1 className="text-2xl text-rose-500">hello</h1>
+      <h1 className="text-2xl text-rose-500">hellso</h1>
+
 
     </div>
   );
